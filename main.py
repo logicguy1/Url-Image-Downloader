d62c0ee1295ef04dd8fb33a1eddc2f7ce350b63e
logicguy1 <56993729+logicguy1@users.noreply.github.com>
2020-10-09 21:03:26 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+
+## Importing Necessary Modules
+import requests # to get image from the web
+import shutil # to save it locally
+
+## Set up the image URL and filename
+image_url = "https://image.shutterstock.com/image-vector/mysterious-computer-hacker-icon-black-260nw-1050727517.jpg"
+filename = image_url.split("/")[-1]
+
+# Open the url image, set stream to True, this will return the stream content.
+r = requests.get(image_url, stream = True)
+
+# Check if the image was retrieved successfully
+if r.status_code == 200:
+    # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+    r.raw.decode_content = True
+
+    # Open a local file with wb ( write binary ) permission.
+    with open(f"downloads/{filename}",'wb') as f:
+        shutil.copyfileobj(r.raw, f)
+
+    # Tell the user if the file was downloaded sucessfully or not
+    print('Image sucessfully Downloaded: ',filename)
+else:
+    print('Image Couldn\'t be retreived')
